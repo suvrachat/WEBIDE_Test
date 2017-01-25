a362218f2b4b7f5aecbbc30681cb7c80862e76a4
Suvrajyoti Chatterjee <suvrajyoti.chatterjee@sap.com>
2017-01-25 06:04:22 +0000

    Template UI5 app

---
+++ LBN_FOC/webapp/controller/Carrier_worklist.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("com.sap.lbnLBN_FOC.controller.Carrier_worklist", {
+
+	});
+});
